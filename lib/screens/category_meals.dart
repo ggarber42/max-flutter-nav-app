c5c78dc3db646746c76b472b283e2d2849e4b52e
ggarber42 <ggarber@protonmail.com>
2022-03-10 23:42:05 -0300

    feat: add meal item

--- lib/screens/category_meals.dart
+++ lib/screens/category_meals.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:nav_app/widgets/meal_item.dart';
+
+import '../utils/dummy_data.dart';
 
 class CategoryMeals extends StatelessWidget {
   static const routes = '/category-meals';
 
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'] as String;
     final categoryTitle = routeArgs['title'] as String;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
-        appBar: AppBar(
-          title: Text(categoryTitle),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('The Recepies for Category'),
-          ),
-        ));
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            title: categoryMeals[index].id,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
   }
 }
