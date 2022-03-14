ffdf6978ad4712d74d0cfd84fdc07bc4dbce398a
ggarber42 <ggarber@protonmail.com>
2022-03-14 20:00:31 -0300

    add pushReplacment in order to avoid big stack

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nav_app/widgets/main_drawer.dart';
 
 class Filters extends StatelessWidget {
   static const routeName = '/filters';
@@ -9,6 +10,7 @@ class Filters extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your Filters'),
       ),
+      drawer: MainDrawer(),
       body: Container(
         child: Center(
           child: Text('Filters'),
